345a3134241c54f6fa6a28a2e94ef224a35e04f9
Marina <marina.ua13@gmail.com>
2024-05-31 16:40:56 +0300

    Solution 2 (add media to .gitignore)

--- cinema/views.py
+++ cinema/views.py
@@ -110,7 +110,6 @@ class MovieViewSet(
     @action(
         methods=["POST"],
         detail=True,
-        # permission_classes=[IsAdminOrIfAuthenticatedReadOnly],
         url_path="upload-image"
     )
     def upload_image(self, request, pk=None):
